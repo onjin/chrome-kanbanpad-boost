523476d84c8548c0df090af524bbefd1669d3c6c
Marek Wywiał <onjinx@gmail.com>
2012-03-19 09:06:48 +0100

    Added: [0.4] store columns state in cookie

--- kanbanpad-boost/kanbanpad_boost.js
+++ kanbanpad-boost/kanbanpad_boost.js
@@ -1,8 +1,38 @@
 $(function() {
+    // BEGIN: column helpers
+    function toggle_column(el) {
+        if (el.hasClass('boost_collapsed_column')) {
+            restore_column(el);
+        } else {
+            collapse_column(el);
+        }
+    }
+    function collapse_column(el) {
+        id = el.attr('id');
+        el.addClass('boost_collapsed_column');
+        $.cookie(id, 'collapsed');
+    }
+    function restore_column(el) {
+        id = el.attr('id');
+        el.removeClass('boost_collapsed_column');
+        $.cookie(id, '');
+    }
+    // END: column helpers
+
+    // restore columns state onload
+    $('.kanban-column').each(function(index) {
+        el = $(this);
+        id = el.attr('id');
+        if ($.cookie(id) == 'collapsed') {
+            collapse_column(el);
+        }
+    });
+
+    // attach columns toggling
     $('.kanban-column').each(function(index) {
         col = $(this);
         col.find('h1.edit-step').click(function() {
-            $(this).parent().parent().toggleClass('boost_collapsed_column');
+            toggle_column($(this).parent().parent());
         });
 
     });
