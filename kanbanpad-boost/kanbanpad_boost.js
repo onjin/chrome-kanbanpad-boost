9cfd2dddc5023a6f36dcd29ba80ae3a16a2e36a2
Marek Wywiał <onjinx@gmail.com>
2012-03-21 12:06:36 +0100

    Changed javascript into module

--- kanbanpad-boost/kanbanpad_boost.js
+++ kanbanpad-boost/kanbanpad_boost.js
@@ -1,40 +1,54 @@
-$(function() {
-    // BEGIN: column helpers
-    function toggle_column(el) {
+var KBPBoost = (function($) {
+    var my = {};
+
+    // public
+    my.toggle_column = function toggle_column(el) {
         if (el.hasClass('boost_collapsed_column')) {
-            restore_column(el);
+            my.restore_column(el);
         } else {
-            collapse_column(el);
+            my.collapse_column(el);
         }
-    }
-    function collapse_column(el) {
+    };
+
+    my.collapse_column = function collapse_column(el) {
         id = el.attr('id');
         el.addClass('boost_collapsed_column');
         $.cookie(id, 'collapsed');
-    }
-    function restore_column(el) {
+    };
+
+    my.restore_column = function restore_column(el) {
         id = el.attr('id');
         el.removeClass('boost_collapsed_column');
         $.cookie(id, '');
-    }
-    // END: column helpers
+    };
 
-    // restore columns state onload
-    $('.kanban-column').each(function(index) {
-        el = $(this);
-        id = el.attr('id');
-        if ($.cookie(id) == 'collapsed') {
-            collapse_column(el);
-        }
-    });
-
-    // attach columns toggling
-    $('.kanban-column').each(function(index) {
-        col = $(this);
-        col.find('h1.edit-step').click(function() {
-            toggle_column($(this).parent().parent());
+    my.init = function() {
+        // restore columns state onload
+        $('.kanban-column').each(function(index) {
+            el = $(this);
+            id = el.attr('id');
+            if ($.cookie(id) == 'collapsed') {
+                my.collapse_column(el);
+            }
         });
 
-    });
+        // attach columns toggling
+        $('.kanban-column').each(function(index) {
+            col = $(this);
+            col.find('h1.edit-step').click(function() {
+                my.toggle_column($(this).parent().parent());
+            });
 
+        });
+    };
+
+    // private
+    //function private() {};
+
+    return my;
+} (jQuery));
+
+// initialize
+$(function() {
+    KBPBoost.init();
 });
